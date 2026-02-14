472b10b61f8774fcc605fcff5a3c883a89121849
yassin549 <khoualdiyassin26@gmail.com>
2026-02-14 10:12:25 +0100

    commit

--- megatron-waitlist/components/layout/RightSidebar.tsx
+++ megatron-waitlist/components/layout/RightSidebar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
 
 // Custom Icons
